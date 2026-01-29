6b3b443ff116f1f9e49937ffbb7df7475551436d
Mateo Chartier <malgawin39@gmail.com>
2026-01-29 14:34:46 +0100

    new service for task + update for stop

--- scripts/ReasonSelection.js
+++ scripts/ReasonSelection.js
@@ -4,12 +4,343 @@
 
 /*$.mockjaxSettings.proxyType = 'POST';*/
 
-var GetReasonSelectionV3_JSON1 = [{ 'Id': 41, 'Display': 'NewReasNewGrp', 'Description': 'A description of new reas new group', 'LongDisplay': 'NewReasNewGrp - ', 'Color': '#FFEE58', 'ReasonScore': 100, 'DetailsRequired': false, 'ReasonGroupDisplay': 'NewGroup', 'ReasonGroupLongDisplay': 'NewGroup - ', 'ReasonGroupId': 28 }, { 'Id': 103, 'Display': 'Test3', 'LongDisplay': 'Test3 - ', 'Color': '#D07D26', 'ReasonScore': 100, 'DetailsRequired': false, 'ReasonGroupDisplay': 'Tests', 'ReasonGroupLongDisplay': 'Tests - ', 'ReasonGroupId': 23 }, { 'Id': 25, 'Display': 'Test1', 'Description': 'A description of Test1', 'LongDisplay': 'Test1 - ', 'Color': '#D07D26', 'ReasonScore': 100, 'DetailsRequired': true, 'ReasonGroupDisplay': 'Tests', 'ReasonGroupLongDisplay': 'Tests - ', 'ReasonGroupId': 23 }, { 'Id': 26, 'Display': 'Test2', 'LongDisplay': 'Test2 - ', 'Color': '#D07D26', 'ReasonScore': 100, 'DetailsRequired': false, 'ReasonGroupDisplay': 'Tests', 'ReasonGroupLongDisplay': 'Tests - ', 'ReasonGroupId': 23 }, { 'Id': 27, 'Display': 'A1', 'Description': 'A description of A1', 'LongDisplay': 'A1 - ', 'Color': '#42A5F5', 'ReasonScore': 100, 'DetailsRequired': false, 'ReasonGroupDisplay': 'A', 'ReasonGroupLongDisplay': 'A - ', 'ReasonGroupId': 24 }, { 'Id': 28, 'Display': 'A2', 'LongDisplay': 'A2 - ', 'Color': '#42A5F5', 'ReasonScore': 100, 'DetailsRequired': false, 'ReasonGroupDisplay': 'A', 'ReasonGroupLongDisplay': 'A - ', 'ReasonGroupId': 24 }, { 'Id': 39, 'Display': 'New reason', 'LongDisplay': 'New reason - Test adding a new reason', 'Description': 'Test adding a new reason', 'Color': '#FFEE58', 'ReasonScore': 100, 'DetailsRequired': false, 'ReasonGroupDisplay': 'A', 'ReasonGroupLongDisplay': 'A - ', 'ReasonGroupId': 24 }, { 'Id': 29, 'Display': 'A3', 'Description': 'A description of A3', 'LongDisplay': 'A3 - ', 'Color': '#42A5F5', 'ReasonScore': 100, 'DetailsRequired': false, 'ReasonGroupDisplay': 'A', 'ReasonGroupLongDisplay': 'A - ', 'ReasonGroupId': 24 }, { 'Id': 29, 'Display': 'A4', 'Description': 'A description of A4', 'LongDisplay': 'A4 - ', 'Color': '#42A5F5', 'ReasonScore': 100, 'DetailsRequired': false, 'ReasonGroupDisplay': 'A', 'ReasonGroupLongDisplay': 'A - ', 'ReasonGroupId': 24 }, { 'Id': 29, 'Display': 'A5', 'Description': 'A description of A5', 'LongDisplay': 'A5 - ', 'Color': '#42A5F5', 'ReasonScore': 100, 'DetailsRequired': false, 'ReasonGroupDisplay': 'A', 'ReasonGroupLongDisplay': 'A - ', 'ReasonGroupId': 24 }, { 'Id': 103, 'Display': 'Test4', 'LongDisplay': 'Test4 - ', 'Color': '#D07D26', 'ReasonScore': 100, 'DetailsRequired': false, 'ReasonGroupDisplay': 'Tests', 'ReasonGroupLongDisplay': 'Tests - ', 'ReasonGroupId': 23 }, { 'Id': 103, 'Display': 'Test5', 'LongDisplay': 'Test5 - ', 'Color': '#D07D26', 'ReasonScore': 100, 'DetailsRequired': false, 'ReasonGroupDisplay': 'Tests', 'ReasonGroupLongDisplay': 'Tests - ', 'ReasonGroupId': 23 }, { 'Id': 103, 'Display': 'Test6', 'LongDisplay': 'Test6 - ', 'Color': '#D07D26', 'ReasonScore': 100, 'DetailsRequired': false, 'ReasonGroupDisplay': 'Tests', 'ReasonGroupLongDisplay': 'Tests - ', 'ReasonGroupId': 23 }];
+var GetReasonSelectionV3_JSON1 = [
+  {
+    'Id': 41,
+    'Display': 'NewReasNewGrp',
+    'Description': 'A description of new reas new group',
+    'LongDisplay': 'NewReasNewGrp - ',
+    'Color': '#FFEE58',
+    'ReasonScore': 100,
+    'DetailsRequired': false,
+    'ReasonGroupDisplay': 'NewGroup',
+    'ReasonGroupLongDisplay': 'NewGroup - ',
+    'ReasonGroupId': 28
+  },
+  {
+    'Id': 103,
+    'Display': 'Test3',
+    'LongDisplay': 'Test3 - ',
+    'Color': '#D07D26',
+    'ReasonScore': 100,
+    'DetailsRequired': false,
+    'ReasonGroupDisplay': 'Tests',
+    'ReasonGroupLongDisplay': 'Tests - ',
+    'ReasonGroupId': 23
+  },
+  {
+    'Id': 25,
+    'Display': 'Test1',
+    'Description': 'A description of Test1',
+    'LongDisplay': 'Test1 - ',
+    'Color': '#D07D26',
+    'ReasonScore': 100,
+    'DetailsRequired': true,
+    'ReasonGroupDisplay': 'Tests',
+    'ReasonGroupLongDisplay': 'Tests - ',
+    'ReasonGroupId': 23
+  },
+  {
+    'Id': 26,
+    'Display': 'Test2',
+    'LongDisplay': 'Test2 - ',
+    'Color': '#D07D26',
+    'ReasonScore': 100,
+    'DetailsRequired': false,
+    'ReasonGroupDisplay': 'Tests',
+    'ReasonGroupLongDisplay': 'Tests - ',
+    'ReasonGroupId': 23
+  },
+  {
+    'Id': 27,
+    'Display': 'A1',
+    'Description': 'A description of A1',
+    'LongDisplay': 'A1 - ',
+    'Color': '#42A5F5',
+    'ReasonScore': 100,
+    'DetailsRequired': false,
+    'ReasonGroupDisplay': 'A',
+    'ReasonGroupLongDisplay': 'A - ',
+    'ReasonGroupId': 24
+  },
+  {
+    'Id': 28,
+    'Display': 'A2',
+    'LongDisplay': 'A2 - ',
+    'Color': '#42A5F5',
+    'ReasonScore': 100,
+    'DetailsRequired': false,
+    'ReasonGroupDisplay': 'A',
+    'ReasonGroupLongDisplay': 'A - ',
+    'ReasonGroupId': 24
+  },
+  {
+    'Id': 39,
+    'Display': 'New reason',
+    'LongDisplay': 'New reason - Test adding a new reason',
+    'Description': 'Test adding a new reason',
+    'Color': '#FFEE58',
+    'ReasonScore': 100,
+    'DetailsRequired': false,
+    'ReasonGroupDisplay': 'A',
+    'ReasonGroupLongDisplay': 'A - ',
+    'ReasonGroupId': 24
+  },
+  {
+    'Id': 29,
+    'Display': 'A3',
+    'Description': 'A description of A3',
+    'LongDisplay': 'A3 - ',
+    'Color': '#42A5F5',
+    'ReasonScore': 100,
+    'DetailsRequired': false,
+    'ReasonGroupDisplay': 'A',
+    'ReasonGroupLongDisplay': 'A - ',
+    'ReasonGroupId': 24
+  },
+  {
+    'Id': 29,
+    'Display': 'A4',
+    'Description': 'A description of A4',
+    'LongDisplay': 'A4 - ',
+    'Color': '#42A5F5',
+    'ReasonScore': 100,
+    'DetailsRequired': false,
+    'ReasonGroupDisplay': 'A',
+    'ReasonGroupLongDisplay': 'A - ',
+    'ReasonGroupId': 24
+  },
+  {
+    'Id': 29,
+    'Display': 'A5',
+    'Description': 'A description of A5',
+    'LongDisplay': 'A5 - ',
+    'Color': '#42A5F5',
+    'ReasonScore': 100,
+    'DetailsRequired': false,
+    'ReasonGroupDisplay': 'A',
+    'ReasonGroupLongDisplay': 'A - ',
+    'ReasonGroupId': 24
+  },
+  {
+    'Id': 103,
+    'Display': 'Test4',
+    'LongDisplay': 'Test4 - ',
+    'Color': '#D07D26',
+    'ReasonScore': 100,
+    'DetailsRequired': false,
+    'ReasonGroupDisplay': 'Tests',
+    'ReasonGroupLongDisplay': 'Tests - ',
+    'ReasonGroupId': 23
+  },
+  {
+    'Id': 103,
+    'Display': 'Test5',
+    'LongDisplay': 'Test5 - ',
+    'Color': '#D07D26',
+    'ReasonScore': 100,
+    'DetailsRequired': false,
+    'ReasonGroupDisplay': 'Tests',
+    'ReasonGroupLongDisplay': 'Tests - ',
+    'ReasonGroupId': 23
+  },
+  {
+    'Id': 103,
+    'Display': 'Test6',
+    'LongDisplay': 'Test6 - ',
+    'Color': '#D07D26',
+    'ReasonScore': 100,
+    'DetailsRequired': false,
+    'ReasonGroupDisplay': 'Tests',
+    'ReasonGroupLongDisplay': 'Tests - ',
+    'ReasonGroupId': 23
+  }
+];
 
 // To change :
-var GetReasonSelectionV3_JSON2 = [{ 'Id': 41, 'Display': 'NewReasNewGrp', 'LongDisplay': 'NewReasNewGrp - ', 'Color': '#FFFF00', 'ReasonScore': 100, 'DetailsRequired': false, 'ReasonGroupDisplay': 'NewGroup', 'ReasonGroupLongDisplay': 'NewGroup - ', 'ReasonGroupId': 28 }, { 'Id': 103, 'Display': 'Test3', 'LongDisplay': 'Test3 - ', 'Color': '#FFFF00', 'ReasonScore': 100, 'DetailsRequired': false, 'ReasonGroupDisplay': 'Tests', 'ReasonGroupLongDisplay': 'Tests - ', 'ReasonGroupId': 23 }, { 'Id': 25, 'Display': 'Test1', 'LongDisplay': 'Test1 - ', 'Color': '#FFFF00', 'ReasonScore': 100, 'DetailsRequired': true, 'ReasonGroupDisplay': 'Tests', 'ReasonGroupLongDisplay': 'Tests - ', 'ReasonGroupId': 23 }, { 'Id': 26, 'Display': 'Test2', 'LongDisplay': 'Test2 - ', 'Color': '#FFFF00', 'ReasonScore': 100, 'DetailsRequired': false, 'ReasonGroupDisplay': 'Tests', 'ReasonGroupLongDisplay': 'Tests - ', 'ReasonGroupId': 23 }, { 'Id': 27, 'Display': 'A1', 'LongDisplay': 'A1 - ', 'Color': '#FFFF00', 'ReasonScore': 100, 'DetailsRequired': false, 'ReasonGroupDisplay': 'A', 'ReasonGroupLongDisplay': 'A - ', 'ReasonGroupId': 24 }, { 'Id': 28, 'Display': 'A2', 'LongDisplay': 'A2 - ', 'Color': '#FFFF00', 'ReasonScore': 100, 'DetailsRequired': false, 'ReasonGroupDisplay': 'A', 'ReasonGroupLongDisplay': 'A - ', 'ReasonGroupId': 24 }, { 'Id': 39, 'Display': 'New reason', 'LongDisplay': 'New reason - Test adding a new reason', 'Description': 'Test adding a new reason', 'Color': '#FFFF00', 'ReasonScore': 100, 'DetailsRequired': false, 'ReasonGroupDisplay': 'A', 'ReasonGroupLongDisplay': 'A - ', 'ReasonGroupId': 24 }, { 'Id': 29, 'Display': 'A3', 'LongDisplay': 'A3 - ', 'Color': '#FFFF00', 'ReasonScore': 100, 'DetailsRequired': false, 'ReasonGroupDisplay': 'A', 'ReasonGroupLongDisplay': 'A - ', 'ReasonGroupId': 24 }];
+var GetReasonSelectionV3_JSON2 = [
+  {
+    'Id': 41,
+    'Display': 'NewReasNewGrp',
+    'LongDisplay': 'NewReasNewGrp - ',
+    'Color': '#FFFF00',
+    'ReasonScore': 100,
+    'DetailsRequired': false,
+    'ReasonGroupDisplay': 'NewGroup',
+    'ReasonGroupLongDisplay': 'NewGroup - ',
+    'ReasonGroupId': 28
+  },
+  {
+    'Id': 103,
+    'Display': 'Test3',
+    'LongDisplay': 'Test3 - ',
+    'Color': '#FFFF00',
+    'ReasonScore': 100,
+    'DetailsRequired': false,
+    'ReasonGroupDisplay': 'Tests',
+    'ReasonGroupLongDisplay': 'Tests - ',
+    'ReasonGroupId': 23
+  },
+  {
+    'Id': 25,
+    'Display': 'Test1',
+    'LongDisplay': 'Test1 - ',
+    'Color': '#FFFF00',
+    'ReasonScore': 100,
+    'DetailsRequired': true,
+    'ReasonGroupDisplay': 'Tests',
+    'ReasonGroupLongDisplay': 'Tests - ',
+    'ReasonGroupId': 23
+  },
+  {
+    'Id': 26,
+    'Display': 'Test2',
+    'LongDisplay': 'Test2 - ',
+    'Color': '#FFFF00',
+    'ReasonScore': 100,
+    'DetailsRequired': false,
+    'ReasonGroupDisplay': 'Tests',
+    'ReasonGroupLongDisplay': 'Tests - ',
+    'ReasonGroupId': 23
+  },
+  {
+    'Id': 27,
+    'Display': 'A1',
+    'LongDisplay': 'A1 - ',
+    'Color': '#FFFF00',
+    'ReasonScore': 100,
+    'DetailsRequired': false,
+    'ReasonGroupDisplay': 'A',
+    'ReasonGroupLongDisplay': 'A - ',
+    'ReasonGroupId': 24
+  },
+  {
+    'Id': 28,
+    'Display': 'A2',
+    'LongDisplay': 'A2 - ',
+    'Color': '#FFFF00',
+    'ReasonScore': 100,
+    'DetailsRequired': false,
+    'ReasonGroupDisplay': 'A',
+    'ReasonGroupLongDisplay': 'A - ',
+    'ReasonGroupId': 24
+  },
+  {
+    'Id': 39,
+    'Display': 'New reason',
+    'LongDisplay': 'New reason - Test adding a new reason',
+    'Description': 'Test adding a new reason',
+    'Color': '#FFFF00',
+    'ReasonScore': 100,
+    'DetailsRequired': false,
+    'ReasonGroupDisplay': 'A',
+    'ReasonGroupLongDisplay': 'A - ',
+    'ReasonGroupId': 24
+  },
+  {
+    'Id': 29,
+    'Display': 'A3',
+    'LongDisplay': 'A3 - ',
+    'Color': '#FFFF00',
+    'ReasonScore': 100,
+    'DetailsRequired': false,
+    'ReasonGroupDisplay': 'A',
+    'ReasonGroupLongDisplay': 'A - ',
+    'ReasonGroupId': 24
+  }
+];
 
-var GetReasonSelectionV3_JSON3 = [{ 'Id': 41, 'Display': 'NewReasNewGrp', 'LongDisplay': 'NewReasNewGrp - ', 'Color': '#FFFF00', 'ReasonScore': 100, 'DetailsRequired': false, 'ReasonGroupDisplay': 'NewGroup', 'ReasonGroupLongDisplay': 'NewGroup - ', 'ReasonGroupId': 28 }, { 'Id': 103, 'Display': 'Test3', 'LongDisplay': 'Test3 - ', 'Color': '#FFFF00', 'ReasonScore': 100, 'DetailsRequired': false, 'ReasonGroupDisplay': 'Tests', 'ReasonGroupLongDisplay': 'Tests - ', 'ReasonGroupId': 23 }, { 'Id': 25, 'Display': 'Test1', 'LongDisplay': 'Test1 - ', 'Color': '#FFFF00', 'ReasonScore': 100, 'DetailsRequired': true, 'ReasonGroupDisplay': 'Tests', 'ReasonGroupLongDisplay': 'Tests - ', 'ReasonGroupId': 23 }, { 'Id': 26, 'Display': 'Test2', 'LongDisplay': 'Test2 - ', 'Color': '#FFFF00', 'ReasonScore': 100, 'DetailsRequired': false, 'ReasonGroupDisplay': 'Tests', 'ReasonGroupLongDisplay': 'Tests - ', 'ReasonGroupId': 23 }, { 'Id': 27, 'Display': 'A1', 'LongDisplay': 'A1 - ', 'Color': '#FFFF00', 'ReasonScore': 100, 'DetailsRequired': false, 'ReasonGroupDisplay': 'A', 'ReasonGroupLongDisplay': 'A - ', 'ReasonGroupId': 24 }, { 'Id': 28, 'Display': 'A2', 'LongDisplay': 'A2 - ', 'Color': '#FFFF00', 'ReasonScore': 100, 'DetailsRequired': false, 'ReasonGroupDisplay': 'A', 'ReasonGroupLongDisplay': 'A - ', 'ReasonGroupId': 24 }, { 'Id': 39, 'Display': 'New reason', 'LongDisplay': 'New reason - Test adding a new reason', 'Description': 'Test adding a new reason', 'Color': '#FFFF00', 'ReasonScore': 100, 'DetailsRequired': false, 'ReasonGroupDisplay': 'A', 'ReasonGroupLongDisplay': 'A - ', 'ReasonGroupId': 24 }, { 'Id': 29, 'Display': 'A3', 'LongDisplay': 'A3 - ', 'Color': '#FFFF00', 'ReasonScore': 100, 'DetailsRequired': false, 'ReasonGroupDisplay': 'A', 'ReasonGroupLongDisplay': 'A - ', 'ReasonGroupId': 24 }];
+var GetReasonSelectionV3_JSON3 = [
+  {
+    'Id': 41,
+    'Display': 'NewReasNewGrp',
+    'LongDisplay': 'NewReasNewGrp - ',
+    'Color': '#FFFF00',
+    'ReasonScore': 100,
+    'DetailsRequired': false,
+    'ReasonGroupDisplay': 'NewGroup',
+    'ReasonGroupLongDisplay': 'NewGroup - ',
+    'ReasonGroupId': 28
+  },
+  {
+    'Id': 103,
+    'Display': 'Test3',
+    'LongDisplay': 'Test3 - ',
+    'Color': '#FFFF00',
+    'ReasonScore': 100,
+    'DetailsRequired': false,
+    'ReasonGroupDisplay': 'Tests',
+    'ReasonGroupLongDisplay': 'Tests - ',
+    'ReasonGroupId': 23
+  },
+  {
+    'Id': 25,
+    'Display': 'Test1',
+    'LongDisplay': 'Test1 - ',
+    'Color': '#FFFF00',
+    'ReasonScore': 100,
+    'DetailsRequired': true,
+    'ReasonGroupDisplay': 'Tests',
+    'ReasonGroupLongDisplay': 'Tests - ',
+    'ReasonGroupId': 23
+  },
+  {
+    'Id': 26,
+    'Display': 'Test2',
+    'LongDisplay': 'Test2 - ',
+    'Color': '#FFFF00',
+    'ReasonScore': 100,
+    'DetailsRequired': false,
+    'ReasonGroupDisplay': 'Tests',
+    'ReasonGroupLongDisplay': 'Tests - ',
+    'ReasonGroupId': 23
+  },
+  {
+    'Id': 27,
+    'Display': 'A1',
+    'LongDisplay': 'A1 - ',
+    'Color': '#FFFF00',
+    'ReasonScore': 100,
+    'DetailsRequired': false,
+    'ReasonGroupDisplay': 'A',
+    'ReasonGroupLongDisplay': 'A - ',
+    'ReasonGroupId': 24
+  },
+  {
+    'Id': 28,
+    'Display': 'A2',
+    'LongDisplay': 'A2 - ',
+    'Color': '#FFFF00',
+    'ReasonScore': 100,
+    'DetailsRequired': false,
+    'ReasonGroupDisplay': 'A',
+    'ReasonGroupLongDisplay': 'A - ',
+    'ReasonGroupId': 24
+  },
+  {
+    'Id': 39,
+    'Display': 'New reason',
+    'LongDisplay': 'New reason - Test adding a new reason',
+    'Description': 'Test adding a new reason',
+    'Color': '#FFFF00',
+    'ReasonScore': 100,
+    'DetailsRequired': false,
+    'ReasonGroupDisplay': 'A',
+    'ReasonGroupLongDisplay': 'A - ',
+    'ReasonGroupId': 24
+  },
+  {
+    'Id': 29,
+    'Display': 'A3',
+    'LongDisplay': 'A3 - ',
+    'Color': '#FFFF00',
+    'ReasonScore': 100,
+    'DetailsRequired': false,
+    'ReasonGroupDisplay': 'A',
+    'ReasonGroupLongDisplay': 'A - ',
+    'ReasonGroupId': 24
+  }
+];
 
 var invalidMachineResponse = {
   'ErrorMessage': 'Invalid machine',
@@ -19,7 +350,7 @@ var invalidMachineResponse = {
 /* &begin=2013-09-03T08:28:37Z&end=2013-09-03T08:37:37Z */
 /* POST = remove &periods%5B0%5D%5Bbegin%5D=2013-09-03T08%3A28%3A37Z&periods%5B0%5D%5Bend%5D=2013-09-03T08%3A37%3A37Z */
 $.mockjax({
-  url: 'http://localhost:8082/ReasonSelection/Post?MachineId=18',
+  url: 'http://localhost:8082/ReasonSelection/Post?MachineId=18&RoleKey=operator',
   type: 'POST',
   contentType: 'text/json',
   dataType: 'json',
@@ -27,19 +358,19 @@ $.mockjax({
   responseText: GetReasonSelectionV3_JSON1
 });
 $.mockjax({
-  url: 'http://localhost:8082/ReasonSelection/Post?MachineId=19',
+  url: 'http://localhost:8082/ReasonSelection/Post?MachineId=19&RoleKey=operator',
   type: 'POST',
   responseTime: 800,
   responseText: GetReasonSelectionV3_JSON2
 });
 $.mockjax({
-  url: 'http://localhost:8082/ReasonSelection/Post?MachineId=20',
+  url: 'http://localhost:8082/ReasonSelection/Post?MachineId=20&RoleKey=operator',
   type: 'POST',
   responseTime: 800,
   responseText: GetReasonSelectionV3_JSON3
 });
 $.mockjax({
-  url: 'http://localhost:8082/ReasonSelection/Post?MachineId=22',
+  url: 'http://localhost:8082/ReasonSelection/Post?MachineId=22&RoleKey=operator',
   type: 'POST',
   responseTime: 1000,
   responseText: invalidMachineResponse
